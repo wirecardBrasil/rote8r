d2ca15806e6ebf73bbdedca9f76493b5533b7509
Fernando Petroni <fernando.petroni@moip.com.br>
2018-04-11 11:50:49 -0300

    Using factory

--- mongorepository/src/main/java/br/com/moip/rote8r/routing/repositories/MongoDBRuleRepository.java
+++ mongorepository/src/main/java/br/com/moip/rote8r/routing/repositories/MongoDBRuleRepository.java
@@ -2,11 +2,11 @@ package br.com.moip.rote8r.routing.repositories;
 
 import br.com.moip.rote8r.routing.models.RoutingRule;
 import br.com.moip.rote8r.routing.service.RoutingRuleRepository;
-import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.data.mongodb.core.query.BasicQuery;
 
 import java.util.List;
@@ -17,7 +17,8 @@ public class MongoDBRuleRepository implements RoutingRuleRepository {
 
     public MongoDBRuleRepository(String mongoUri) {
         MongoClientURI mongoClientURI = new MongoClientURI(mongoUri);
-        this.mongoOperations = new MongoTemplate(new MongoClient(mongoClientURI), mongoClientURI.getDatabase());
+        SimpleMongoDbFactory factory = new SimpleMongoDbFactory(mongoClientURI);
+        this.mongoOperations = new MongoTemplate(factory);
     }
 
     public MongoDBRuleRepository(MongoOperations mongoOperations) {
