4e85c22c94621c811ab5cb8b7d39c1a73d705ab3
Fernando Petroni <fernando.petroni@moip.com.br>
2018-04-11 15:23:32 -0300

    Adding routingRuleCollectionName

--- mongorepository/src/main/java/br/com/moip/rote8r/routing/repositories/MongoDBRuleRepository.java
+++ mongorepository/src/main/java/br/com/moip/rote8r/routing/repositories/MongoDBRuleRepository.java
@@ -14,19 +14,22 @@ import java.util.List;
 public class MongoDBRuleRepository implements RoutingRuleRepository {
 
     private MongoOperations mongoOperations;
+    private String routingRuleCollectionName;
 
-    public MongoDBRuleRepository(String mongoUri) {
+    public MongoDBRuleRepository(String mongoUri, String routingRuleCollectionName) {
         MongoClientURI mongoClientURI = new MongoClientURI(mongoUri);
         SimpleMongoDbFactory factory = new SimpleMongoDbFactory(mongoClientURI);
         this.mongoOperations = new MongoTemplate(factory);
+        this.routingRuleCollectionName = routingRuleCollectionName;
     }
 
-    public MongoDBRuleRepository(MongoOperations mongoOperations) {
+    public MongoDBRuleRepository(MongoOperations mongoOperations, String routingRuleCollectionName) {
         this.mongoOperations = mongoOperations;
+        this.routingRuleCollectionName = routingRuleCollectionName;
     }
 
     public List<RoutingRule> findAllRulesInPrecedenceOrder() {
         BasicQuery query = (BasicQuery) new BasicQuery("{}").with(new Sort(Sort.Direction.ASC, "precedence"));
-        return mongoOperations.find(query, RoutingRule.class);
+        return mongoOperations.find(query, RoutingRule.class, routingRuleCollectionName);
     }
 }
